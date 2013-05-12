e9be17b2518d68d16f39b94d96cfd8f25ca07e82
Brandon <bleventh@gmail.com>
2013-05-12 07:23:35 -0700

    refactored

--- premierScores.rb
+++ premierScores.rb
@@ -1,12 +1,13 @@
+#!/usr/bin/env ruby 
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
 puts "Positon - Team - League Points"
-puts "--------------------------------"
+puts "-" * 30
 page = "http://www.premierleague.com/en-gb/matchday/league-table.html"
 doc = Nokogiri::HTML(open(page))
 doc.css(".club-row").each do |item|
   puts item.at_css('.col-pos').text + " " + item.at_css('.col-club').text + " " + item.at_css('.col-pts').text
-puts "--------------------------------"
+puts "-" * 30
 end
